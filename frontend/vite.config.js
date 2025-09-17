95f1b9b3cd3e932e5a6850279caf35e583b1b85d
Julian <figueredo.julian96@gmail.com>
2025-09-17 16:02:23 -0500

    modificacion de archivos docker frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,
+   watch: {
+      usePolling: true // Para hot reload en Docker (especialmente Windows)
+    }
+  },
+  build: {
+    outDir: 'dist', // Vite usa 'dist' por defecto
+    sourcemap: false, // Desactivar en producción
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173
+  }
 })
